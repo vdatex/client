52a167d386d55413e5c53aff7a9474643eed5503
Silas Silva <silas.silva@rockt.com.br>
2017-12-09 00:18:11 -0200

    Alterando migration para não setar banco diretamente e pegar dinamicamente.

--- src/main/resources/migration/V1__CREATE_tb_client.sql
+++ src/main/resources/migration/V1__CREATE_tb_client.sql
@@ -1,8 +1,4 @@
--- Table: clients.tb_client
-
--- DROP TABLE clients.tb_client;
-
-CREATE TABLE clients.tb_client
+CREATE TABLE tb_client
 (
   id_client uuid NOT NULL,
   st_email character varying(255),
